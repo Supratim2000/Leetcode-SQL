1cd15de37ceaec3d8c25d7bd5daec349f5efd196
Supratim2000 <52972176+Supratim2000@users.noreply.github.com>
2022-06-13 18:23:59 +0530

    Update and rename Department Highest Salary to Department Highest Salary.sql

---
+++ Department Highest Salary.sql
@@ -0,0 +1,1 @@
+SELECT d.name AS Department, e.name AS Employee, e.salary AS Salary FROM Employee AS e JOIN Department AS d ON e.departmentId=d.id WHERE (e.departmentId,e.salary) IN(SELECT e.departmentId,MAX(e.salary) FROM Employee AS e GROUP BY e.departmentId);
